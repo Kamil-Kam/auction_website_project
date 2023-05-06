c916f2a5feb58d9d1120ea1fab59ce439ed31e42
Kamil73 <kamil.bylinka9@gmail.com>
2023-05-07 00:06:39 +0100

    first commit

--- auction_website_project/auction_website/models.py
+++ auction_website_project/auction_website/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=120, null=False, blank=False)
+
+    def __str__(self):
+        return self.category
+
+
+class Stan(models.Model):
+    stan = models.CharField(max_length=120, null=False, blank=False)
+
+    def __str__(self):
+        return self.stan
+
+
+class Item(models.Model):
+    description = models.TextField(max_length=2500)
+    title = models.CharField(max_length=120)
+    price = models.IntegerField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    stan = models.ForeignKey(Stan, on_delete=models.SET_NULL)
+    location = models.CharField(max_length=240)
+
+    def __str__(self):
+        return self.title
+
+
--- auction_website_project/auction_website/views.py
+++ auction_website_project/auction_website/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+import models
 
 # Create your views here.
+
+
+def main_page(request):
+    pass
+
+
+def categories_view(request):
+    pass
+
+
+def single_category(request):
+    pass
+
+
+def add_item(request):
+    pass
+
+
+def view_item(request):
+    pass
+
+
+def item_photo(request):
+    pass
+
+
+def log_in(request):
+    pass
+
+
+def account(request):
+    pass
+
+
+
