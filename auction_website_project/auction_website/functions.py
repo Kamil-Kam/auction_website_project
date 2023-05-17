512992bca7f7ed8df837ffd4e77027e6bc039db5
Kamil73 <kamil.bylinka9@gmail.com>
2023-05-18 00:25:24 +0100

    implementation of built-in AbstractUser instead of Account model

--- auction_website_project/auction_website/functions.py
+++
@@ -1,7 +0,0 @@
-def get_image_path(instance, filename):
-    category_name_path = instance.category.category_name
-    return f"{category_name_path}/{filename}"
-
-
-def get_avatars_path(instance, filename):
-    return f"avatars/{filename}"
